2b394d3ce9142d64fad72f6a0b9ebdf95a26f0d1
uuganbold <uuganhuu@gmail.com>
2020-01-22 11:49:57 -0600

    singly linked list

---
+++ basic/src/linkedlist/LinkedList.ts
@@ -0,0 +1,148 @@
+/**
+ * 
+ */
+class LinkedListNode<T> {
+  public data: T;
+  public next: LinkedListNode<T>;
+
+  constructor(data: T) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+export class LinkedList<T> {
+  private head: LinkedListNode<T>;
+  private size:number;
+
+  constructor() {
+    this.head = null;
+    this.size=0;
+  }
+
+  /**
+   * Time complexity will be O(1)
+   * @param data Data to be added to the list
+   */
+  public add(data: T): void{
+    const node = new LinkedListNode(data);
+    node.next = this.head;
+    this.head = node;
+    this.size=this.size+1;
+  }
+
+  /**
+   * Time complexity is O(n) where n is size of the list
+   * @param index index where new node to be added
+   * @param data  data belonging to the new node.
+   */
+  public addAt(index:number,data:T){
+      if(index<0||index>this.size){
+         throw new RangeError('The member cannot be added at out of the size');
+      }
+      
+      const newNode=new LinkedListNode(data);
+      if(index==0) {
+        newNode.next=this.head;
+        this.head=newNode;
+      }else{
+          let node=this.head;
+          while(index>1){
+            node=node.next;
+            index=index-1;
+          }
+          newNode.next=node.next;
+          node.next=newNode;
+      }
+      this.size=this.size+1;
+  }
+
+  /**
+   * Time complexity O(1)
+   */
+  public clear(){
+      this.head=null;
+  }
+
+  /**
+   * Time complexity O(n)
+   * @param data 
+   */
+  public contains(data:T):boolean{
+    let node=this.head;
+    while(node!=null&&node.data!=data){
+      node=node.next;
+    }
+    return node!=null;
+  }
+
+  /**
+   * Time complexity O(1)
+   */
+  public isEmpty():boolean{
+    return this.size==0;
+  }
+
+  public getSize():number{
+      return this.size;
+  }
+
+  /**
+   * Time complexity O(n)
+   * @param data 
+   */
+  public remove(data:T):boolean{
+    let node=this.head;
+    let parent=null;
+    while(node!=null&&node.data!=data){
+        parent=node;
+        node=node.next;
+    }
+    if(node==null) return false;
+    if(parent!=null) parent.next=node.next;
+    else this.head=node.next;
+    this.size=this.size-1;
+    return true;
+  }
+
+  public removeAt(index:number):boolean{
+    if(index<0||index>=this.size) return false;
+    let node=this.head;
+    let parent=null;
+    while(index>0){
+        parent=node;
+        node=node.next;
+        index=index-1;
+    }
+    if(parent!=null) parent.next=node.next;
+    else this.head=node.next;
+    this.size=this.size-1;
+    return true;
+  }
+
+  public get(index:number):T{
+      let node=this.head;
+      while(node!=null&&index>0){
+        node=node.next;
+        index=index-1;
+      }
+      if(node==null) return null;
+      else return node.data;
+  }
+
+  public peek(): T {
+    if(this.head==null) return null;
+    else return this.head.data;
+  }
+
+  public toString():string{
+     let result="[";
+     let node=this.head;
+     while(node!=null){
+        result+=node.data+"->";
+        node=node.next;
+     }
+     result+="]";
+     return result;
+  }
+}
---
+++ basic/test/linkedlist/LinkedList.test.ts
@@ -0,0 +1,127 @@
+import { expect } from 'chai';
+import {LinkedList} from '../../src/linkedlist/LinkedList';
+
+describe('LinkedList', () => {
+    it('should size increase and change head after add element', () => {
+        const result = new LinkedList<Number>();
+        result.add(10);
+        expect(result.getSize()).to.be.equal(1);
+        expect(result.peek()).to.be.equal(10);
+        result.add(20)
+        expect(result.getSize()).to.be.equal(2);
+        expect(result.peek()).to.be.equal(20);
+    });
+
+    it('should size increased and change element after addAt, cannot be overflowed',()=>{
+        const result=new LinkedList<Number>();
+        result.add(10);
+        result.add(20);
+        result.add(30);
+        result.addAt(0,40);
+        expect(result.getSize()).to.be.equal(4);
+        expect(result.get(0)).to.be.equal(40);
+        expect(result.get(1)).to.be.equal(30);
+        result.addAt(2,65);
+        expect(result.getSize()).to.be.equal(5);
+        expect(result.get(2)).to.be.equal(65);
+        expect(result.get(1)).to.be.equal(30);
+        expect(result.get(3)).to.be.equal(20);
+        result.addAt(5,77);
+        expect(result.getSize()).to.be.equal(6);
+        expect(result.get(5)).to.be.equal(77);
+        expect(result.get(4)).to.be.equal(10);
+        expect(result.get(6)).to.be.null;
+
+        expect(()=>result.addAt(-1,40)).to.throw(RangeError);
+        expect(()=>result.addAt(7,40)).to.throw(RangeError);
+    })
+
+    it('should contain if it is added',()=>{
+        const result=new LinkedList<Number>();
+        result.add(10);
+        result.addAt(0,20);
+        result.addAt(2,40);
+
+        expect(result.contains(10)).to.be.true;
+        expect(result.contains(40)).to.be.true;
+        expect(result.contains(20)).to.be.true;
+
+    });
+
+    it('should note contain if it is not added',()=>{
+        const result=new LinkedList<Number>();
+        result.add(10);
+        result.addAt(0,20);
+        result.addAt(2,40);
+
+        expect(result.contains(60)).to.be.false;
+
+    });
+
+    it('should be removed. size and links should be consistent',()=>{
+        const result=new LinkedList<Number>();
+        result.add(10);
+        result.add(30);
+        result.add(50);
+        result.add(70)
+        expect(result.remove(30)).to.be.true;
+        expect(result.getSize()).to.be.equal(3);
+        expect(result.get(2)).to.be.equal(10);
+
+        expect(result.remove(70)).to.be.true;
+        expect(result.getSize()).to.be.equal(2);
+        expect(result.peek()).to.be.equal(50);
+
+        expect(result.remove(10)).to.be.true;
+        expect(result.getSize()).to.be.equal(1);
+        expect(result.peek()).to.be.equal(50);
+    })
+
+    it('should not be removed if it is not contains the element',()=>{
+        const result=new LinkedList<Number>();
+        expect(result.remove(30)).to.be.false;
+        expect(result.getSize()).to.be.equal(0);
+        result.add(10);
+        result.add(30);
+        result.add(50);
+        result.add(70);
+        expect(result.remove(40)).to.be.false;
+        expect(result.getSize()).to.be.equal(4);
+    })
+    
+    it('shoule be removed by index if is belongs to the list',()=>{
+        const result=new LinkedList<Number>();
+        result.add(10);
+        result.add(30);
+        result.add(50);
+        result.add(70);
+        result.add(90);
+
+        expect(result.removeAt(2)).to.be.true;
+        expect(result.getSize()).to.be.equal(4);
+        expect(result.get(2)).to.be.equal(30);
+        expect(result.get(1)).to.be.equal(70);
+
+        expect(result.removeAt(3)).to.be.true;
+        expect(result.getSize()).to.be.equal(3);
+        expect(result.get(3)).to.be.null;
+
+        expect(result.removeAt(0)).to.be.true;
+        expect(result.getSize()).to.be.equal(2);
+        expect(result.peek()).to.be.equal(70);
+    });
+
+    it('should not be deleted if index is out of the size',()=>{
+        const result=new LinkedList<Number>();
+        result.add(10);
+        result.add(30);
+        result.add(50);
+        result.add(70);
+        result.add(90);
+
+        expect(result.removeAt(-1)).to.be.false;
+        expect(result.removeAt(5)).to.be.false;
+        expect(result.removeAt(6)).to.be.false;
+    })
+});
+
