6cb64d7c1c2f3d6441bfc753d0effc8ebb2cc2c8
uuganbold <uuganhuu@gmail.com>
2020-03-05 10:10:47 -0600

    css change

--- bluedit/src/pages/components/Header.tsx
+++ bluedit/src/pages/components/Header.tsx
@@ -1,48 +1,55 @@
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, Form, Input, DropdownMenu, DropdownItem } from "reactstrap"
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, Form, Input, DropdownMenu, DropdownItem, Collapse, NavbarToggler } from "reactstrap"
 import classnames from 'classnames';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as fa from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
 const Header=()=>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
     return (
-        <Navbar tag='header' expand={true} dark={true} className={classnames('flex-column','flex-md-row','bd-navbar')}>
+        <Navbar tag='header' expand="md" dark={true} className={classnames('bd-navbar')}>
             <NavbarBrand className={classnames('mr-0','mr-md-2')} href='/' aria-label='Bluedit'>
                 <FontAwesomeIcon icon={fa.faRedditAlien}/>
             </NavbarBrand>
-            <div className='navbar-nav-scroll'>
-                <Nav navbar className={classnames('bd-navbar-nav','flex-row')}>
-                    <NavItem>
-                        <NavLink active>Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink active>Groups</NavLink>
-                    </NavItem>
-                    <UncontrolledDropdown nav>
-                        <DropdownToggle nav caret className={'mr-md-2'}>
-                            Sort By
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem >Recent</DropdownItem>
-                            <DropdownItem>Action</DropdownItem>
-                            <DropdownItem>Another Action</DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
+            <NavbarToggler onClick={toggle}/>
+            <Collapse navbar isOpen={isOpen}>
+            
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink active>Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>Groups</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav>
+                            <DropdownToggle nav caret >
+                                Sort By
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem >Recent</DropdownItem>
+                                <DropdownItem>Action</DropdownItem>
+                                <DropdownItem>Another Action</DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                <Form inline className='ml-md-auto'>
+                    <Input type='search' placeholder='Search'/>
+                </Form>
+                <Nav navbar>
+                        <UncontrolledDropdown nav >
+                            <DropdownToggle nav caret>
+                                Uuganbold
+                            </DropdownToggle>
+                            <DropdownMenu>
+                                <DropdownItem >Recent</DropdownItem>
+                                <DropdownItem>Action</DropdownItem>
+                                <DropdownItem>Another Action</DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
                 </Nav>
-            </div>
-            <Form inline className={classnames('my-2','my-lg-0','ml-md-auto')}>
-                <Input className={'mr-sm-2'} type='search' placeholder='Search'/>
-            </Form>
-            <Nav navbar className={classnames('flex-row', 'd-none', 'd-md-flex')}>
-                    <UncontrolledDropdown nav >
-                        <DropdownToggle nav caret className={classnames('p-2','mr-md-2')}>
-                            Uuganbold
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem >Recent</DropdownItem>
-                            <DropdownItem>Action</DropdownItem>
-                            <DropdownItem>Another Action</DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-            </Nav>
+            </Collapse>
+            
         </Navbar>
         
     )
